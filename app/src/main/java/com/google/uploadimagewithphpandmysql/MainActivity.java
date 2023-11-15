cf900af8eb8349e640dc8f8e3408899c767e5055
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-11-15 19:31:35 +0700

    add Request Volley

--- app/src/main/java/com/google/uploadimagewithphpandmysql/MainActivity.java
+++ app/src/main/java/com/google/uploadimagewithphpandmysql/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -15,15 +16,24 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
 import android.util.Base64;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
@@ -77,6 +87,28 @@ public class MainActivity extends AppCompatActivity {
                     byte[] bytes = byteArrayOutputStream.toByteArray();
                     final String base64Image = Base64.encodeToString(bytes, Base64.DEFAULT);
                     RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                    String url = "";
+
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+
+                                }
+                            }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    }){
+                        @Nullable
+                        @Override
+                        protected Map<String, String> getParams(){
+                            Map<String, String> paramV = new HashMap<>();
+                            paramV.put("param", "abc");
+                            return paramV;
+                        }
+                    };
                 }
                 else Toast.makeText(MainActivity.this, "Select the image first", Toast.LENGTH_SHORT).show();
             }
