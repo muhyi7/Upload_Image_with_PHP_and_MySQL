c87ce20816c1ca5a928fb993fb5835a665ce8d59
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-11-15 18:28:44 +0700

    compress image

--- app/src/main/java/com/google/uploadimagewithphpandmysql/MainActivity.java
+++ app/src/main/java/com/google/uploadimagewithphpandmysql/MainActivity.java
@@ -15,9 +15,16 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.URI;
+import android.util.Base64;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -57,6 +64,21 @@ public class MainActivity extends AppCompatActivity {
 //                Intent intent = new Intent(Intent.ACTION_PICK);
 //                intent.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 //                activityResultLauncher.launch(intent);
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ByteArrayOutputStream byteArrayOutputStream;
+                byteArrayOutputStream = new ByteArrayOutputStream();
+                if (bitmap != null){
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+                    byte[] bytes = byteArrayOutputStream.toByteArray();
+                    final String base64Image = Base64.encodeToString(bytes, Base64.DEFAULT);
+                    RequestQueue queue = Volley
+                }
+                else Toast.makeText(MainActivity.this, "Select the image first", Toast.LENGTH_SHORT).show();
             }
         });
     }
